781129a79aa0b59405036fc80fe588cab78838b8
nadafruksana <42529671+nadafruksana@users.noreply.github.com>
2020-04-07 13:05:15 +0530

    Update Posts.js

--- src/Posts.js
+++ src/Posts.js
@@ -1,34 +1,54 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { startGetPosts } from './actions/postsAction'
-// npm install axios 
-import { Link } from 'react-router-dom'
+//import axios from 'axios'
+import {Link} from 'react-router-dom'
+import {startGetPosts} from './actions/postsAction'
+import {connect} from 'react-redux'
 
-class Posts extends React.Component {
-    componentDidMount() {
-        if (this.props.posts.length === 0) {
-            this.props.dispatch(startGetPosts())
-        }
+
+class Posts extends React.Component{
+   
+    componentDidMount(){
+      if(this.props.posts.length===0){
+          this.props.dispatch(startGetPosts())
+      }
     }
-    
-    render(){ 
+
+    render(){
         return (
-            <div>
-                <h1>Listing Posts - {this.props.posts.length} </h1>
-                <ul>
-                    {this.props.posts.map(post => {
-                        return <li key={post.id}><Link to={`/posts/${post.id}`}> {post.title} </Link> </li>
-                    })}
-                </ul>
-            </div>
+        <div>
+
+            <h1> Total Posts :  {this.props.posts.length}</h1>
+            <ul>
+                {
+                    this.props.posts.map((post, i)=>{
+                return <li key={post.id}><Link to={`/posts/${post.id}`}>
+                        {post.title}</Link></li>
+                    })
+                }
+            </ul>
+        </div>
+
+
         )
     }
-    
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state)=>{
     return {
-        posts: state.posts
+        posts : state.posts
     }
 }
+
 export default connect(mapStateToProps)(Posts)
+
+
+
+
+
+
+
+
+
+
+
+  
